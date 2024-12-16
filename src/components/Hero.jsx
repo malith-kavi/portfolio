9be45be028e39cd9eaa6525f294248e3f255be09
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-17 00:29:07 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ export const Hero = () => {
                         initial="hidden"
                         animate="visible"
                         className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-                        Kevin Rush
+                        Malith Kavinda
                     </motion.h1>
                     <motion.span 
                         variants={container(0.5)}

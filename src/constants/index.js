2bb2635e07ca3c4221ceb02a60274c666860b9a9
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2025-02-04 10:04:26 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,36 +16,6 @@ export const HERO_CONTENT = `I would like to learn new technologies and programm
 
 export const ABOUT_TEXT = `I’m an undergraduate at the University of Colombo, passionate about software development. I’ve worked with technologies like Flutter, React, Swift, Android Studio, JavaScript, and Python to build innovative web and mobile applications.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
 
 export const PROJECTS = [
   {
@@ -55,14 +25,6 @@ export const PROJECTS = [
       "The Weather app is a user-friendly application that provides real-time weather information for any location. It allows users to search for cities and view current weather conditions, including temperature, humidity, and wind speed. The app is designed to offer accurate and up-to-date weather data, helping users plan their day effectively",
     technologies: ["Swift", "Weather API"],
     link: "https://github.com/malith-kavi/Weather"
-  },
-  {
-    title: "To Do Mobile Application",
-    image: todo,
-    description:
-      "A simple and efficient task management app to help users organize, track, and complete daily tasks. Key features include task creation, editing and real-time progress tracking with secure user authentication.",
-    technologies: ["Java"],
-    link:"https://github.com/malith-kavi/Task-Track-Mobile-Application"
   },
   {
     title: "Food Suggestion Mobile Application",
